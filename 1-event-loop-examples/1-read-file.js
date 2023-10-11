473fd1efeaf8669e8118e6dddc6bce07a4439aab
Hòa Đặng <dangnhathoa22@gmail.com>
2023-10-11 08:58:47 +0700

    event loop

---
+++ 1-event-loop-examples/1-read-file.js
@@ -0,0 +1,11 @@
+const {readFile} = require('fs');
+console.log('started a first task');
+readFile('D:/code/web/LBMS/content/first.txt','utf8',(err,result)=>{
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log(result);
+    console.log('complete first task');
+})
+console.log('starting next task');
